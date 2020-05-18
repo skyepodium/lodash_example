8b7c21570ce2dc27c4cd97ad6e78d61b454a62ad
skyepodium <skyepodium@gmail.com>
2020-05-18 23:19:11 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    main
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  created() {
+    this.lodashExample()
+  },
+  methods: {
+    lodashExample() {
+
+      if(_.isEqual('안녕하세요', 'Hello')) {
+        console.log('same')
+      }
+      if(_.isEmpty([])) {
+        console.log('empty')
+      }
+
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+// 파일 위치: /root/src/main.js
+
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+// lodash를 불러옵니다.
+import 'lodash'
 
 new Vue({
   render: h => h(App),
